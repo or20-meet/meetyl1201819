b4d4460d175f365b41dd22c75eca690746fe8b38
or20-meet <or20@meet.mit.edu>
2018-10-21 12:26:27 -0400

    lab 3

---
+++ lab3.py
@@ -0,0 +1,12 @@
+import turtle
+while 0=0:
+	turtle.pendown()
+	turtl.right(10)
+	turtle.foward(200)
+	turtle.right(135)
+	turtle.foward(100)
+	turtle.right(90)
+	turtle.forward(50)
+	turtle.penup()
+	turtle.home()
+turtle.mainloop()
