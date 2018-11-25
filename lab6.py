023a3b5b8bc597200b3eab750217286038d28999
or20-meet <or20@meet.mit.edu>
2018-11-25 12:02:37 -0500

    lab6

--- lab6.py
+++ lab6.py
@@ -1,7 +1,19 @@
+from turtle import Turtle
 import turtle
-class Square(turtle):
-	def __init__(self, size):
-		self.shape("Square")
-		self.size =size
+import random
 
+class Square(Turtle):
+	def __init__(self, sz):
+		Turtle.__init__(self)
+		self.sz= sz
+		self.shape("square")
+		self.shapesize(sz)
 
+	def random_color(self):
+		colors= ["red", "orange", "pink"]
+		c= random.randint(0,2)
+		self.color(colors[c])
+
+s= Square(5)
+s.random_color()
+turtle.mainloop()
