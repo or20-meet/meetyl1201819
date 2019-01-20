f399790eaac2285dfb17d34ed32c0c4aabd28f83
or20-meet <or20@meet.mit.edu>
2019-01-20 12:16:56 -0500

    final project

--- personal_project/agar_io.py
+++ personal_project/agar_io.py
@@ -26,9 +26,10 @@ balls=[]
 
 my_ball= Ball(0, 0, 2, 2, 50, "green")
 
-for i in range(number_of_balls):
-	x=  random.randint(maximum_ball_radius-screen_width, screen_width-maximum_ball_radius)
-	y= random.randint(maximum_ball_radius - screen_height, screen_height- maximum_ball_radius)
+#the following function randomize the values of x, y, dx, dy, r, color
+def randomize(ball):
+	x= random.randint(maximum_ball_radius-screen_width, screen_width-maximum_ball_radius)
+	y=random.randint(maximum_ball_radius - screen_height, screen_height- maximum_ball_radius)
 	dx=0
 	while dx==0:
 		dx= random.randint(minimum_ball_dx  , maximum_ball_dx)
@@ -37,7 +38,15 @@ for i in range(number_of_balls):
 		dy= random.randint(minimum_ball_dy, maximum_ball_dy)
 	radius= random.randint(minimum_ball_radius, maximum_ball_radius)
 	color= (random.random(), random.random(), random.random())
-	new_ball= Ball(x, y, dx, dy, radius, color)
+	ball.goto(x,y)
+	ball.dx=dx
+	ball.dy=dy
+	ball.r= radius
+	ball.color=color
+
+for i in range(number_of_balls):
+	new_ball= Ball(1,1,1,1,1,"green")
+	randomize(new_ball)
 	balls.append(new_ball)
 
 for ball in balls:
@@ -53,12 +62,44 @@ def collide(ball1, ball2):
 		return False
 
 
+
 def check_all_collision():
 	for ball1 in balls:
 		for ball2 in balls:
 			if collide(ball1, ball2):
 				rad1= ball1.r
 				rad2= ball2.r 
-				if ball1.r> ball2.r:
-					ball2= Ball()
+				if rad1> rad2:
+					randomize(ball2)
+					ball1.r+=1
+				elif rad2> rad1:
+					randomize(ball1)
+					ball2.r+=1
+
+
+
+def check_my_collision():
+	for ball in balls:
+		if collide(ball,my_ball):
+			my_rad= my_ball.r
+			ball_rad= ball.r
+			if my_rad>ball_rad:
+				my_ball.r+=1
+				randomize(ball)
+
+			elif ball_rad>my_rad:
+				return False
+	return True
+
+
+
+def movearound(event):
+	x= event.x-screen_width
+	y= screen.height-event.y
+	my_ball.goto(event.x, event.y)
+
+
+turtle.getcanvas().bind("<motion>",movearound)
+turtle.listen()
+movearound()
 turtle.mainloop()
